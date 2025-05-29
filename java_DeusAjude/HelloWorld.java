c6cd94652ec7daf995fd089ebfeec7625450e314
EUGENIO BOUDAHER ROCHA <eugeniorocha0286@digitalcollege.internal>
2025-05-28 21:15:08 -0300

    comecou o java

---
+++ java_DeusAjude/HelloWorld.java
@@ -0,0 +1,18 @@
+package java_DeusAjude;
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro núemro");
+        int numero1 = scanner.nextInt();
+        
+        System.out.println("Digite o segindo núemro");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+
+        System.out.println(soma);
+        
+}
+}
